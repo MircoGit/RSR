7eb19a9356da2e1533ee4ce096c7c63850168061
damiano.renfer <damiano.renfer@he-arc.ch>
2014-02-20 14:42:10 +0100

    Initial commit

---
+++ RSR/OpenCVTemplate.cpp
@@ -0,0 +1,19 @@
+// OpenCVTemplate.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include "opencv2\opencv.hpp"
+
+using namespace cv;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	Mat img;
+	img = imread("opencv.jpg");
+	imshow("OpenCVTemplate", img);
+
+	waitKey();
+
+	return 0;
+}
+
